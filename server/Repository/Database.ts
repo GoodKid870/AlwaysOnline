1f8a029e814e21e81487269e2eac5366206719c4
Daniil <51880793+GoodKid870@users.noreply.github.com>
2023-07-07 01:40:25 +0300

    13

--- server/Repository/Database.ts
+++ server/Repository/Database.ts
@@ -8,7 +8,7 @@ class Database {
         //мы берем какой-то обьект из базы данных с которым хотим работать
         public static GetDatabaseObject(type, id){
             try {
-                const data: string = fs.readFileSync(`./server/database/${type}/${id}.json`, "utf-8");
+                const data: string = fs.readFileSync(`./server/Database/${type}/${id}.json`, "utf-8");
                 return JSON.parse(data);
             } catch (e) {
                 return undefined;
@@ -18,7 +18,7 @@ class Database {
         //мы создаем какой-то обьект в базе данных с которым хотим работать
         public static SetDatabaseObject(type, id, object){
             try {
-                fs.writeFileSync(`./server/database/${type}/${id}.json`, JSON.stringify(object, null, 2));
+                fs.writeFileSync(`./server/Database/${type}/${id}.json`, JSON.stringify(object, null, 2));
             } catch (e) {
                 console.log(e);
             }
@@ -27,9 +27,9 @@ class Database {
         //мы берем всех пользователей из базы данных с которым хотим работать
         public static GetAllDatabaseUsers(currentUser) {
             try {
-                const files: string[] = fs.readdirSync('./server/database/users/');
+                const files: string[] = fs.readdirSync('./server/Database/users/');
                 const users: {userInfo: any, avatar: any}[] = files.map((file) => {
-                    const filePath: string = path.join('./server/database/users/', file);
+                    const filePath: string = path.join('./server/Database/users/', file);
                     const data: string = fs.readFileSync(filePath, 'utf8');
                     const { userInfo, avatar } = JSON.parse(data);
                     return { userInfo, avatar };
@@ -43,7 +43,7 @@ class Database {
         //мы берем сообщения из базы данных с которым хотим работать
         public static GetDatabaseMessageObject(type, id){
             try {
-                const data = fs.readFileSync(`./server/database/${type}/${id}.json`, "utf-8")
+                const data = fs.readFileSync(`./server/Database/${type}/${id}.json`, "utf-8")
                 return JSON.parse(data)
             } catch (e) {
                 return undefined
@@ -53,7 +53,7 @@ class Database {
         //мы создаем сообщения в базе данных с которым хотим работать
         public static SetDatabaseMessageObjects(type, filename, object){
             try {
-                fs.writeFileSync(`./server/database/${type}/${filename}.json`, JSON.stringify(object, null, 2));
+                fs.writeFileSync(`./server/Database/${type}/${filename}.json`, JSON.stringify(object, null, 2));
             } catch (e) {
                 console.log(e)
             }
@@ -62,7 +62,7 @@ class Database {
         //мы меняем пользователя в базе данных
          public static ChangeDatabaseUser(type, filename, newUserMail){
             try {
-                fs.renameSync(`./server/database/${type}/${filename}.json`, `./server/database/${type}/${newUserMail}.json` )
+                fs.renameSync(`./server/Database/${type}/${filename}.json`, `./server/database/${type}/${newUserMail}.json` )
             } catch (e) {
                 console.log(e)
             }
